bae096739aeebf907baafb411f1b3ed540581968
Joel Meador <joel@expectedbehavior.com>
2011-05-11 17:37:30 -0400

    initial

---
+++ Gemfile
@@ -0,0 +1,20 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.1.0.beta1'
+gem 'sqlite3'
+gem 'sass'
+gem 'coffee-script'
+gem 'uglifier'
+gem 'jquery-rails'
+gem 'awesome_print'
+
+group :development, :test do
+  gem 'rb-fsevent', :require => false if RUBY_PLATFORM =~ /darwin/i
+  gem 'guard-test'
+  gem 'guard-livereload'
+end
+
+group :test do
+  # Pretty printed test output
+  gem 'turn', :require => false
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GuardDemo::Application.config.secret_token = 'eb1c20fe6e63a482d943c71c3f2d09499e7790262de4fad50ab1efc95b435d81508cc78d680d80d4ec27a34d90e349c5c3e808a77a9a31f6f075807b5e3e1cd3'
